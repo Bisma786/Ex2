1fb33a09336957e06b4dff9b5625192a68e26f38
Bisma786 <159830145+Bisma786@users.noreply.github.com>
2024-02-26 17:23:55 +0500

    Initial commit

---
+++ ex2.js
@@ -0,0 +1,2 @@
+var yourname = "Sir Zia";
+console.log("Hello ".concat(yourname, ", would ypu like to learn some Python today?"));
---
+++ ex2.ts
@@ -0,0 +1,4 @@
+let yourname:string="Sir Zia"
+console.log(`Hello ${yourname}, would ypu like to learn some Python today?`)
+//Q:2 is successfully completed
+//Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
